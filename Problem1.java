3fbd6f663edcd753b0f633bd7ba4305d649c8c54
Yamini <yamini.bommineni1@t-mobile.com>
2024-06-29 15:09:23 -0700

    Completed Competitive Coding 1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,24 @@
-
+// "static void main" must be defined in a public class.
+//Time complexity : O(logn) - Binery search on input array
+//Space Complecity: O(1) - no new datastructure created
+public class Problem1 {
+    //{1,3,4,5,6,7,8,9}
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,5};
+        // missingNum(nums);
+        // public void missingNum(int[] nums){
+            int low = 0;
+            int high = nums.length-1;
+            while(low<=high){
+                int mid = low + (high-low)/2;
+                if(nums[mid]!=mid+1){  
+                    // if(mid==0 && nums[mid]!=1) System.out.println(1);
+                    // if(mid!=0 && nums[mid-1] == mid)   System.out.println(mid+1);
+                    high = mid-1;
+                } else{
+                    low=mid+1;
+                }
+            }
+             System.out.println(low+1);
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,16 @@
 
+//Time complexity : O(N) - iterating through whole input array
+//Space Complecity: O(1) - no new datastructure created
+// "static void main" must be defined in a public class.
+public class Problem2 {
+    public static void main(String[] args) {
+        //Find missing integer in an unsorted array
+        int n=8;
+        int sum = (n*(n+1))/2;
+        int[] nums2= {1,2,8,5,7,6,4};
+        for(int i=0;i<n-1;i++){
+            sum=sum-nums2[i];
+        }  
+        System.out.println(sum);
+    }
+}
